f3a0513ff4bfe52d192db0c0bcb329a18bfc923c
William Rocha <william.bueno.rocha@gmail.com>
2023-02-20 20:18:32 -0300

    Implement new messages for fun

--- bot/bot_client.py
+++ bot/bot_client.py
@@ -1,4 +1,4 @@
-import discord
+import discord, random
 
 class BotClient(discord.Client):
     async def on_ready(self):
@@ -22,5 +22,24 @@ class BotClient(discord.Client):
         # Debug printing
         print(f"{username} said: '{user_message}' ({channel})")
 
+        # Just for fun
+        if message.content.startswith('!gc') or message.content.startswith('!rank'):
+            if username == 'willzera':
+                await message.reply(f'Your GC level is 20!', mention_author=True)
+                return
+            if username == 'danshimo':
+                await message.reply(f'Your GC level is 20!', mention_author=True)
+                return
+            if username == 'Nkc.Yuji':
+                await message.reply(f'Your GC level is 18!', mention_author=True)
+                return
+            if username == 'CrazyLog':
+                await message.reply(f'Your GC level is 2!', mention_author=True)
+                return
+            await message.reply(f'Your GC level is {random.randint(0, 20)}!', mention_author=True)
+
         if message.content.startswith('!hello') or message.content.startswith('!ola') or message.content.startswith('!olá'):
             await message.reply(f'Hello {username}!', mention_author=True)
+
+        if message.content.startswith('!help'):
+            await message.reply(f'No help!', mention_author=False)
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from bot.bot_client import BotClient
-import discord
+import os, discord
+
+discord_token = os.environ.get('DISCORD_TOKEN')
 
 def main():
     intents = discord.Intents.default()
     intents.message_content = True
     
     client = BotClient(intents=intents)
-    client.run(TOKEN)
+    client.run(discord_token)
 
 if __name__ == '__main__':
     main()
